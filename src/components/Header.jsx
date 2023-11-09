a548fa9d88b2158c1d64afef2688bdc9c612849b
Brian Simiyu <briansimiyuj@gmail.com>
2023-11-09 10:35:43 +0300

    Inline styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = ({ title }) =>{
 
         <header>
         
-            <h1>{title}</h1>
+            <h1 style={{color: "red", backgroundColor: "yellow"}}>{title}</h1>
         
         </header>
 
