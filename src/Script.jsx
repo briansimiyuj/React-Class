2d9a8e0f4a03c7d4cfcde8f8350c1125f594ceef
Brian Simiyu <briansimiyuj@gmail.com>
2023-11-09 09:50:38 +0300

    Creating Components

--- src/Script.jsx
+++ src/Script.jsx
@@ -1,20 +1,12 @@
+import Header from './components/Header'
+
 function Script() {
-
-  const name = 'Brian',
-
-         x = true
 
   return (
 
     <div className="script">
 
-      <h1>Hello world</h1>
-
-      <h2>Hello {name}</h2>
-
-      <h2>{2 + 2}</h2>
-
-      <h2>{x ? 'Yes' : 'No'}</h2>
+      <Header/>
 
     </div>
    
