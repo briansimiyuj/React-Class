ce692b5bcc83c1bd36ffe1632edeb83a69f2f5b3
Brian Simiyu <briansimiyuj@gmail.com>
2023-11-09 08:04:25 +0300

    Expressions in JSX

--- src/Script.jsx
+++ src/Script.jsx
@@ -1,9 +1,20 @@
 function Script() {
+
+  const name = 'Brian',
+
+         x = true
+
   return (
 
     <div className="script">
 
       <h1>Hello world</h1>
+
+      <h2>Hello {name}</h2>
+
+      <h2>{2 + 2}</h2>
+
+      <h2>{x ? 'Yes' : 'No'}</h2>
 
     </div>
    
