be90fe7ce09bf2de837fafe7326dcdcaaafdb7b8
Brian Simiyu <briansimiyuj@gmail.com>
2023-11-10 09:18:20 +0300

    Events as props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,6 @@
 import PropTypes from 'prop-types'
 
-const Button = ({ color, text }) =>{
-
-    const onClick = () =>{
-    
-       console.log('working')
-    
-    }
-
+const Button = ({ color, text, onClick }) =>{
 
     return <button onClick={onClick} className="btn" style={{ backgroundColor: color }}>{text}</button>
 
@@ -23,7 +16,9 @@ Button.propTypes ={
 
     text: PropTypes.string,
 
-    color: PropTypes.string
+    color: PropTypes.string,
+
+    onClick: PropTypes.func
 
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,20 @@ import Button from './Button'
 
 const Header = ({ title }) =>{
 
+    const onClick = () =>{
+    
+       console.log('working')
+    
+    }
+
+
     return(
 
         <header className="header">
         
             <h1>{title}</h1>
             
-            <Button color="green" text="Add" />
+            <Button color="green" text="Add" onClick={onClick}/>
         
         </header>
 
