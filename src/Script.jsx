f8d59bb8f22a53a4b7f8fde81a7666f362e89fac
Brian Simiyu <briansimiyuj@gmail.com>
2023-11-10 10:03:57 +0300

    Prop Drilling

--- src/Script.jsx
+++ src/Script.jsx
@@ -1,7 +1,34 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function Script() {
+
+  const [tasks, setTasks] = useState([
+
+    {
+      "id": 1,
+      "text": "Doctors Appointment",
+      "day": "Feb 5th at 2:30pm",
+      "reminder": true
+    },
+
+    {
+      "id": 2,
+      "text": "Meeting at School",
+      "day": "Feb 6th at 1:30pm",
+      "reminder": true
+    },
+
+    {
+      "id": 3,
+      "text": "Food Shopping",
+      "day": "Feb 7th at 2:30pm",
+      "reminder": false
+    }
+    
+  ])
+
 
   return (
 
@@ -9,7 +36,7 @@ function Script() {
 
       <Header title='Task Tracker'/>
 
-      <Tasks/>
+      <Tasks tasks={tasks}/>
 
     </div>
    
