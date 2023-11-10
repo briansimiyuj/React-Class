f0a29964c2249c00efd46036252e62436ab2b9f9
Brian Simiyu <briansimiyuj@gmail.com>
2023-11-10 09:01:02 +0300

    Reusable components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
-import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) =>{
 
     return(
 
-        <header>
+        <header className="header">
         
             <h1>{title}</h1>
+            
+            <Button color="green" text="Hello" />
+            
+            <Button color="blue" text="Hello1" />
+            
+            <Button color="red" text="Hello2" />
         
         </header>
 
