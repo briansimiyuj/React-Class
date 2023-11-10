00f0fe311ff9fd66934024d7eb03dbf242d9f5fe
Brian Simiyu <briansimiyuj@gmail.com>
2023-11-10 09:43:00 +0300

    Creating a list with map

--- src/Script.jsx
+++ src/Script.jsx
@@ -1,4 +1,5 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function Script() {
 
@@ -7,6 +8,8 @@ function Script() {
     <div className="script">
 
       <Header title='Task Tracker'/>
+
+      <Tasks/>
 
     </div>
    
