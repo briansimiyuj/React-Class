aa5ba5e9a8fdba2bdbd03051b95d3a65364c8a44
Brian Simiyu <briansimiyuj@gmail.com>
2023-11-18 12:31:43 +0300

    Footer and About markup

--- src/Script.jsx
+++ src/Script.jsx
@@ -1,6 +1,8 @@
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import About from './components/About'
 import { useState, useEffect } from 'react'
 
 function Script() {
@@ -131,6 +133,8 @@ function Script() {
          'No tasks to show'
 
       }
+
+      <Footer/>
 
     </div>
    
